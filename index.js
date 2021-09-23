c7bb44c0a8c0eddc40abc361d0effae7bac8fd30
Tani Kaye <1tanikaye@gmail.com>
2021-09-23 12:16:47 -0400

    assignment completed

---
+++ index.js
@@ -0,0 +1,48 @@
+function add() {
+  return (number += 5);
+}
+
+function subtract() {
+  return (number += 5);
+}
+
+function multiply() {
+  return (number += 5);
+}
+
+
+function divide() {
+  return (number /= 3);
+}
+
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function increment(n) {
+  return n + 1
+}
+
+function decrement(n) {
+  return n - 1
+}
+
+function makeInt(string) {
+  return parseInt(string, 10);
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string);
+}
